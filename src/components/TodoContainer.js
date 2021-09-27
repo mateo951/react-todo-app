424a5f3091ef28ea509c8f246a8394a597d84c28
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-09-27 12:49:57 -0500

    Add todo list main functionality

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,60 @@
+import React from "react"
+import TodosList from "./TodosList";
+import Header from "./Header"
+
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+  handleChange = id => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo
+      }),
+    }))
+  };
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
+      </div>
+    )
+  }
+}
+export default TodoContainer
