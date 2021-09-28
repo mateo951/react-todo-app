6728a7f957d6f225d6e675550b0a5c387449704f
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-09-27 20:20:17 -0500

    Delete default files

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -50,11 +50,11 @@ class TodoContainer extends React.Component {
       ]
     });
   };
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(response => response.json())
-      .then(data => this.setState({ todos: data }));
-  }
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ todos: data }));
+  // }
   componentDidMount() {
     const temp = localStorage.getItem("todos")
     const loadedTodos = JSON.parse(temp)
